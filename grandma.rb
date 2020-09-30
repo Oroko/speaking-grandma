53b47f523e3cedc9ac0e386c93152fe5d85cb912
Oroko <aoroko09@gmail.com>
2020-09-30 06:16:02 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (phrase == "Hi Nana, how are you?" || phrase == "Hi!")
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif (phrase == "WHAT DID YOU EAT TODAY?" || phrase == "WHAT?")
+  elsif (phrase == phrase.upcase)
     return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
